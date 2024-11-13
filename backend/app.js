66c920c84ec814c384bf931afb230ff94be15f84
bhargav-quadbtech <bhargav.vermavathsavai@quadbtech.com>
2024-11-13 21:56:09 +0530

    final commit

--- backend/app.js
+++ backend/app.js
@@ -11,12 +11,6 @@ const userRoutes = require('./routes/user');
 
 app.use(cors());
 
-// Or, enable CORS for a specific origin (e.g., frontend URL)
-// app.use(cors({ origin: 'http://localhost:3000' }));
-
-// Your other routes and middlewares
-
-
 
 
 app.use('/api/admin', adminRoutes);
@@ -68,17 +62,16 @@ app.use(express.json());
 
 
 app.use("/files",express.static("files"))
-// MongoDB connection and server initialization
+
 const initializeDBAndServer = async () => {
 
- const username = encodeURIComponent(process.env.username);
-    const password = encodeURIComponent(process.env.password);
+ const username = encodeURIComponent("bhargavcoding");
+    const password = encodeURIComponent("bv@9177221342");
     const uri = `mongodb+srv://${username}:${password}@cluster0.ki5m4.mongodb.net/ecs?retryWrites=true&w=majority&tls=true&appName=Cluster0`;
   
     try {
         await mongoose.connect(uri);
         console.log("Connected to MongoDB...");
-        // Start the server after a successful database connection
         server.listen(9000, () => {
             console.log('Server running on port: 9000');
         });
@@ -89,54 +82,4 @@ const initializeDBAndServer = async () => {
 };
 
 initializeDBAndServer();
-
-const multer = require("multer");
-
-// Multer setup for file uploads
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./files");
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now();
-    cb(null, uniqueSuffix + file.originalname);
-  },
-});
-
-const upload = multer({ storage: storage });
-
-// Ensure PdfDetails model is defined
-require("./pdfDetails");
-const PdfSchema = mongoose.model("PdfDetails");
-
-// Route to upload files
-app.post("/upload-files", upload.single("file"), async (req, res) => {
-  const title = req.body.title;
-  const fileName = req.file.filename;
-  console.log(title);
-  console.log(fileName)
-
-  try {
-    await PdfSchema.create({ title: title, pdf: fileName });
-    res.send({ status: "ok" });
-  } catch (error) {
-    res.status(500).json({ status: "error", message: error.message });
-  }
-});
-
-// Route to get files
-app.get("/get-files", async (req, res) => {
-  try {
-    const data = await PdfSchema.find({});
-    res.send({ status: "ok", data: data });
-  } catch (error) {
-    res.status(500).json({ status: "error", message: error.message });
-  }
-});
-
-// Test route
-app.get("/", (req, res) => {
-  res.send("Success!!!!!!");
-});
-
 
